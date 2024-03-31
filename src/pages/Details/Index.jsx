d72f430c6a653a90200b8e73d99344886e45fef7
Arlan Figueiredo <estudosarlan@gmail.com>
2024-03-31 09:03:23 -0300

    config component button

--- src/pages/Details/Index.jsx
+++ src/pages/Details/Index.jsx
@@ -1,9 +1,13 @@
 import { Container } from './style'
+import { Button } from '../../components/Button'
 
 export function Index(){
   return (
     <Container>
-      <h1>app</h1>
+      <h1>Hello world!</h1>
+      <Button title="Entrar" loading/>
+      <Button title="Cadastrar"/>
+      <Button title="Voltar"/>
     </Container>
   )
 }
