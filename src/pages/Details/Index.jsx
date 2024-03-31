f1780e066134fdea4a1f53d3f6fcd95d4d4ee256
Arlan Figueiredo <estudosarlan@gmail.com>
2024-03-31 17:55:28 -0300

    atualized files

--- src/pages/Details/Index.jsx
+++ src/pages/Details/Index.jsx
@@ -1,13 +1,10 @@
 import { Container } from './style'
-import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
 
 export function Index(){
   return (
     <Container>
-      <h1>Hello world!</h1>
-      <Button title="Entrar" loading/>
-      <Button title="Cadastrar"/>
-      <Button title="Voltar"/>
+      <Header/>
     </Container>
   )
 }
