76ef97b54fa4f9ea6dbbb1603b6c0b20ea800c89
Arlan Figueiredo <estudosarlan@gmail.com>
2024-03-31 08:32:08 -0300

    Creating styles and theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme.js'
+import GlobalStyles from './styles/global.js'
 import { Index } from './pages/Details/Index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Index />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <Index />
+    </ThemeProvider>
   </React.StrictMode>,
 )
